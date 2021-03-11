553b97def7c6d023ce25e24e05a65af0a2ac9077
adam040992 <adam040992@gmail.com>
2021-03-11 18:02:31 +0100

    Building Header Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h2>Lets Build Gmail</h2>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton } from '@material-ui/core';
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://cdn.freelogovectors.net/svg07/new_gmail_logo.svg"/>
+
+            </div>
+            <div className='header__middle'>
+
+
+            </div>
+            <div className='header__right'>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
