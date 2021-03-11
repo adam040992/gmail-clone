7bc3dadc5acc0de4c032f875243c9dc8f0f5e11a
adam040992 <adam040992@gmail.com>
2021-03-11 18:36:46 +0100

    Header building - complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { Avatar, IconButton } from '@material-ui/core';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -14,12 +18,19 @@ function Header() {
 
             </div>
             <div className='header__middle'>
-
+                <SearchIcon />
+                <input placeholder="Search mail" type="text"/>
+                <ArrowDropDownIcon className="header__inputCaret"/>
 
             </div>
             <div className='header__right'>
-
-
+            <IconButton>
+                <AppsIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsIcon />
+            </IconButton>
+            <Avatar />
             </div>
         </div>
     )
