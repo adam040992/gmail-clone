3e4460755d88318eac9691178a22102df325ef69
adam040992 <adam040992@gmail.com>
2021-03-16 17:42:59 +0100

    Creatin mail tools header

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,4 +1,5 @@
 import { IconButton } from '@material-ui/core';
+import { useHistory } from 'react-router-dom'; 
 import React from 'react';
 import './Mail.css';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -10,13 +11,18 @@ import WatchLaterIcon from '@material-ui/icons/WatchLater';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
+import PrintIcon from '@material-ui/icons/Print';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 function Mail() {
+    const history = useHistory();
+    
     return (
         <div className="mail">
             <div className="mail__tools">
                 <div className="mail__toolsLeft">
-                    <IconButton>
+                    <IconButton onClick={() => history.push("/")}>
                         <ArrowBackIcon />
                     </IconButton>
 
@@ -54,10 +60,33 @@ function Mail() {
 
                 </div>
                 <div className="mail__toolsRight">
+                    <IconButton>
+                        <UnfoldMoreIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <PrintIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ExitToAppIcon />
+                    </IconButton>
 
                 </div>
             </div>
-            <h1>Mail</h1>
+
+            <div className="mail__body">
+                <div className="mail__bodyHeader">
+                    <h2>Subject</h2>
+                    <LabelImportantIcon className="mail__important"/>
+                    <p>Title</p>
+                    <p>10pm</p> 
+                </div>
+
+                <div className="mail__message">
+
+                </div>
+            </div>
         </div>
     )
 }
